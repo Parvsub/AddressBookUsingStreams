c31f75aef23782e67242159a1af4780a69668ebe
Parvsub <Praveenboda421@gmail.com>
2023-03-12 16:47:43 +0530

    Added: Address Book Dictionary is created

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,6 @@
 package AddressBookUsingStreams;
 
-import java.util.ArrayList;
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
 
 //Address book program
 public class AddressBook extends ContactDetails{
@@ -11,7 +9,15 @@ public class AddressBook extends ContactDetails{
    public static void add(){
       Scanner scanner = new Scanner(System.in);
       System.out.println("Enter first name");
-      newPerson.setFirstName(scanner.nextLine());
+      String firstname = scanner.nextLine();
+      Optional<ContactDetails> isEmpty=person.stream()
+              .filter(name -> firstname.equals(name.getFirstName()))
+              .findAny();
+      if(!isEmpty.isEmpty()){
+         System.out.println("name already exist");
+         return;
+      }
+      newPerson.setFirstName(firstname);
       System.out.println("Enter last name");
       newPerson.setLastName(scanner.nextLine());
       System.out.println("Enter address");
@@ -28,7 +34,8 @@ public class AddressBook extends ContactDetails{
       newPerson.setEmail(scanner.nextLine());
       ContactDetails contactPerson = new ContactDetails(newPerson.getFirstName(),newPerson.getLastName(), newPerson.getAddress(), newPerson.getCity(), newPerson.getState(), newPerson.getZip(), newPerson.getPhoneNumber(),newPerson.getEmail());
       person.add(contactPerson);
-      System.out.println(person);
+      person.forEach(System.out::println);
+//        System.out.println(person);
    }
 
    public static void modify(){
@@ -90,27 +97,83 @@ public class AddressBook extends ContactDetails{
          System.out.println("record not found");
       }
    }
-   public static void main(String[] args) {
+   public static void countByCity(){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter city name");
+      String inputCity = scanner.next();
+      long count = person.stream()
+              .filter(city -> inputCity.equals(city.getCity()))
+              .count();
+      System.out.println("Total details as per city "+inputCity+ " are: "+count);
+   }
+   //count details as per city
+   public static void countByState(){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter city name");
+      String inputState = scanner.next();
+      long count = person.stream()
+              .filter(state -> inputState.equals(state.getState()))
+              .count();
+      System.out.println("Total details as per state "+inputState+ " are: "+count);
+   }
+   //sort details as per name
+   public static void sortByName(){
+      System.out.println("Sorted list is\n");
+      person.stream()
+              .sorted(Comparator.comparing(ContactDetails::getFirstName))
+              .forEach(System.out::println);
+   }
+   //sort details as per city
+   public static void sortByCity() {
+      System.out.println("Sorted list is\n");
+      person.stream()
+              .sorted(Comparator.comparing(ContactDetails::getCity))
+              .forEach(System.out::println);
+   }
+   //sort details as per state
+   public static void sortByState() {
+      System.out.println("Sorted list is\n");
+      person.stream()
+              .sorted(Comparator.comparing(ContactDetails::getState))
+              .forEach(System.out::println);
+   }
+
+   public void addContacts(AddressBook addressBook) {
       Scanner scanner = new Scanner(System.in);
       int flag = 0;
       int choice;
       System.out.println("Welcome to Address Book Program");
-
       while (flag != 1) {
          System.out.println("Enter 1 to add contact details");
          System.out.println("Enter 2 to edit details");
          System.out.println("Enter 3 to delete contact");
          System.out.println("Enter 4 to exit");
+         System.out.println("Enter 5 to count detail by city");
+         System.out.println("Enter 6 to count detail by state");
+         System.out.println("Enter 7 to count sort by name");
+         System.out.println("Enter 8 to count detail by city");
+         System.out.println("Enter 9 to count detail by state");
          choice = scanner.nextInt();
          scanner.nextLine();
-         if (choice == 1) {
-            add();
-         } else if (choice == 2) {
-            modify();
-         } else if (choice==3) {
-            delete();
-         } else if (choice == 4) {
-            flag = 1;
+         switch (choice) {
+            case 1: add();
+               break;
+            case 2: modify();
+               break;
+            case 3: delete();
+               break;
+            case 4: flag = 1;
+               break;
+            case 5: countByCity();
+               break;
+            case 6: countByState();
+               break;
+            case 7: sortByName();
+               break;
+            case 8: sortByCity();
+               break;
+            case 9: sortByState();
+               break;
          }
       }
    }
